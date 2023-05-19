44b7a848f85a27f7d00f2ca5ae2a0cfa8bafdeec
Jose Lazo <jjlazo79@gmail.com>
2023-05-19 13:03:04 +0200

    Create run.sql

---
+++ run.sql
@@ -0,0 +1,10 @@
+DELETE
+FROM wp_posts
+WHERE post_type = "revision"
+  AND ID NOT IN(SELECT ID
+                FROM (SELECT ID
+                      FROM wp_posts
+                      WHERE post_type = 'revision'
+                      ORDER BY ID
+                      DESC LIMIT 5)
+                 rev)
