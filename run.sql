6cb50b13e15cc9aae585824e0af87d1e53d07e47
Jose Lazo <jjlazo79@gmail.com>
2023-09-07 14:53:49 +0200

    Update run.sql

--- run.sql
+++ run.sql
@@ -22,29 +22,46 @@ WHERE posts.ID IS NULL
 
 
 
--- Extract posts with feature image and metayoast
+-- Extract posts with feature image, categories, tags and metayoast
 SELECT 
  p.ID,
  p.post_content,
  p.post_title,
  p.post_excerpt,
  p.post_name as url,
+ p.post_date,
  users.user_nicename as author,
  pm2.meta_value as metadesc,
  pm3.meta_value as metatitle,
+ GROUP_CONCAT(DISTINCT c.`name`) as categories,
+GROUP_CONCAT(DISTINCT t.`name`) as tags,
  (SELECT guid
-        FROM   wp_posts
-        WHERE  id = pm4.meta_value) AS image
+    FROM   wp_posts
+    WHERE  id = pm4.meta_value) AS image
 FROM wp_posts p
 LEFT JOIN wp_users users
- ON p.post_author = users.ID
+    ON p.post_author = users.ID
 LEFT JOIN wp_postmeta pm2
- ON p.ID = pm2.post_id
- AND pm2.meta_key = '_yoast_wpseo_metadesc'
+    ON p.ID = pm2.post_id
+    AND pm2.meta_key = '_yoast_wpseo_metadesc'
 LEFT JOIN wp_postmeta pm3
- ON p.ID = pm3.post_id
- AND pm3.meta_key = '_yoast_wpseo_title'
+    ON p.ID = pm3.post_id
+    AND pm3.meta_key = '_yoast_wpseo_title'
 LEFT JOIN wp_postmeta pm4
- ON p.ID = pm4.post_id
- AND pm4.meta_key = '_thumbnail_id'
+    ON p.ID = pm4.post_id
+    AND pm4.meta_key = '_thumbnail_id'
+LEFT JOIN wp_term_relationships cr
+    ON (p.`id`=cr.`object_id`)
+LEFT JOIN wp_term_taxonomy ct
+    ON (ct.`term_taxonomy_id`=cr.`term_taxonomy_id`
+    AND ct.`taxonomy`='category')
+LEFT JOIN wp_terms c
+    ON (ct.`term_id`=c.`term_id`)
+LEFT JOIN wp_term_relationships tr
+    ON (p.`id`=tr.`object_id`)
+LEFT JOIN wp_term_taxonomy tt
+    ON (tt.`term_taxonomy_id`=tr.`term_taxonomy_id`
+    AND tt.`taxonomy`='post_tag')
+LEFT JOIN wp_terms t
+    ON (tt.`term_id`=t.`term_id`)
 WHERE post_type = 'post' AND post_status = 'publish'
