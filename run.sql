d604b1cd5ff264a5a4102c6577478b63de72b07b
Jose Lazo <jjlazo79@gmail.com>
2023-09-07 14:24:00 +0200

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,4 +1,4 @@
-## Clear all revisions except last 5
+-- Clear all revisions except last 5
 DELETE
 FROM wp_posts
 WHERE post_type = "revision"
@@ -9,13 +9,16 @@ WHERE post_type = "revision"
                       ORDER BY ID
                       DESC LIMIT 5)
                  rev)
-                 
-## Delete orphans postmetas
+
+
+
+-- Delete orphans postmetas
 DELETE postmeta
 FROM wp_postmeta AS postmeta
 	LEFT JOIN wp_posts AS posts
 	ON posts.ID = postmeta.post_id
 WHERE posts.ID IS NULL
+
 
 
 
